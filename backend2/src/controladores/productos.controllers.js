99cc9e8ce6312ee7502231e750d5a47168c985ff
yaelmedina32 <yael_medina9@outlook.com>
2022-11-02 17:45:21 -0600

    se agrego otro h1 app.component.html

--- backend2/src/controladores/productos.controllers.js
+++ backend2/src/controladores/productos.controllers.js
@@ -3,7 +3,7 @@ import { conectarBD } from '../BD/db.js';
 
 export const getPrueba = async (req, res) =>{
     const pool = await conectarBD();
-    const result = await pool.request().query("SELECT * FROM dbo.cuadrillas where cuadrillaid = 2");
+    const result = await pool.request().query("SELECT * FROM dbo.cuadrillas");
     res.json(result.recordset);
     console.log(result.recordset);
 }
